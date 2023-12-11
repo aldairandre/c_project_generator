dcbfb37fbb93736edc73bded0edcd3a2848e0de6
aldairandre <aldairandre99@gmail.com>
2023-12-11 17:29:43 +0100

    :recycle: Refactor code

--- src/const.js
+++ src/const.js
@@ -5,18 +5,13 @@ const urlRepo = "https://github.com/aldairandre/c_template_for_vscode.git";
 
 const nameRepo = "c_template_for_vscode";
 
-const projectFolder = {
-  en: "project_C",
-  pt: "projectos_C",
-};
-
 const typeOS = os.platform();
+
 const locale = intl.DateTimeFormat().resolvedOptions().locale;
 
 module.exports = {
   urlRepo,
   nameRepo,
-  projectFolder,
   typeOS,
   locale
 };
